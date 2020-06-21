ef4ed8e5271ce94752a91a6bea106c9646f6ccd2
SilentDepth <silentdepth@outlook.com>
2020-06-21 15:50:55 +0800

    feat(style): use Inter and Noto Sans SC from Google Fonts as preferred font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,36 @@
 module.exports = {
+  theme: {
+    fontFamily: {
+      sans: [
+        'Inter',
+        '-apple-system',
+        'Noto Sans',
+        'Helvetica Neue',
+        'Helvetica',
+        'Nimbus Sans L',
+        'Arial',
+        'Liberation Sans',
+        'PingFang SC',
+        'Hiragino Sans GB',
+        'Noto Sans CJK SC',
+        'Source Han Sans SC',
+        'Source Han Sans CN',
+        'Noto Sans SC',
+        'Microsoft YaHei',
+        'Wenquanyi Micro Hei',
+        'WenQuanYi Zen Hei',
+        'ST Heiti',
+        'SimHei',
+        'WenQuanYi Zen Hei Sharp',
+        'sans-serif',
+      ],
+    },
+    fontWeight: {
+      normal: '400',
+      bold: '700',
+      black: '900',
+    },
+  },
   purge: [
     './**/*.vue',
   ],
